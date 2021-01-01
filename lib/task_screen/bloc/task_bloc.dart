0b327d296f8c28ddb8a1544974992b36d1b22d71
Pawel Przetacznik <przetacznikp@gmail.com>
2021-01-01 17:48:44 +0100

    filter tasks according to day

--- lib/task_screen/bloc/task_bloc.dart
+++ lib/task_screen/bloc/task_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:schedule_tasks/const.dart';
 import 'package:schedule_tasks/models/task_model.dart';
 import 'package:schedule_tasks/services/local_data_source.dart';
 import 'package:meta/meta.dart';
@@ -13,8 +14,22 @@ class TaskBloc extends Bloc<TaskEvent, TaskState> {
   TaskBloc() : super(TaskLoading());
 
   LocalDataSource _localDataSource = LocalDataSource();
-  DateTime dateTime = DateTime.now();
-  List<Task> tasks = [];
+  DateTime _dateTime = DateTime.now();
+  List<Task> _tasks = [];
+
+  List<Task> _filter() {
+    List<Task> filtered = [];
+    String weekDay = weekDaysShort[_dateTime.weekday - 1];
+    for (int i = 0; i <= _tasks.length - 1; i++) {
+      for (int j = 0; j <= _tasks[i].weekDays.length - 1; j++) {
+        if (weekDay == _tasks[i].weekDays[j]) {
+          filtered.add(_tasks[i]);
+          break;
+        }
+      }
+    }
+    return filtered;
+  }
 
   @override
   Stream<TaskState> mapEventToState(
@@ -23,18 +38,19 @@ class TaskBloc extends Bloc<TaskEvent, TaskState> {
     if (event is LoadAllTasks) {
       try {
         yield TaskLoading();
-        tasks = await _localDataSource.getTasks();
-        yield MainScreen(tasks: tasks, date: dateTime);
+        _tasks = await _localDataSource.getTasks();
+        yield MainScreen(tasks: _filter(), date: _dateTime);
       } on CacheException {
         // yield EmptyScreen();
-        yield MainScreen(tasks: tasks, date: dateTime);
+        yield MainScreen(tasks: _filter(), date: _dateTime);
       }
     } else if (event is DisplayAddTaskScreen) {
       yield AddScreen();
+      _filter();
     } else if (event is AddNewTask) {
-      tasks.add(event.task);
-      _localDataSource.cacheData(tasks);
-      yield MainScreen(tasks: tasks, date: dateTime);
+      _tasks.add(event.task);
+      _localDataSource.cacheData(_tasks);
+      yield MainScreen(tasks: _filter(), date: _dateTime);
     }
   }
 }
